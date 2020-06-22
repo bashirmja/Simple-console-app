fa8c03b8b6388d0a386fd45ce4ad18ad7f587d48
bashir <bashir.momen@3shape.com>
2020-06-22 14:03:08 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		AAA a= ReturnNull();
+		Test(a);
+		Console.Write(a.B);
+	}
+
+    private static AAA ReturnNull()
+    {
+        return null;
+    }
+
+    public static void Test(AAA a)
+	{
+		if (a== null)
+		{
+			a= new AAA();
+		}
+	}
+}
+
+public class AAA
+{
+	public AAA(){
+		A="a";
+		B="b";
+	}
+
+    public string A { get; set; }
+	public string B { get; set; }
+}
